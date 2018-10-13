2270aa8428d75f9c8a4e898f804141ddd52abb64
Simon Aarons <40786398+simonmkwii-dev@users.noreply.github.com>
2018-10-14 10:53:51 +1100

    Add files via upload

---
+++ Main.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SXDetect
+{
+    public static class Detect
+    {
+        public static bool IsUserRunningSXOS()
+        {
+            var FoundSXOS = false;
+            foreach (var Drive in DriveInfo.GetDrives())
+                if (File.Exists($"{Drive.RootDirectory}/boot.dat"))
+                    FoundSXOS = true;
+            return FoundSXOS;
+        }
+    }
+}
